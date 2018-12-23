fcd5c6f4d624552091c7b3b3c6460b9fa67a2e84
Panayot Angarev <pancho.angarev@gmail.com>
2018-12-23 11:03:21 +0200

    Add contact list to state

--- contact-list/src/App.js
+++ contact-list/src/App.js
@@ -1,38 +1,40 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 
-const contacts = [
-  {
-    "id": "karen",
-    "name": "Karen Isgrigg",
-    "handle": "@karen_isgrigg",
-    "avatarURL": "http://localhost:5001/karen.jpg"
-  },
-  {
-    "id": "richard",
-    "name": "Richard Kalehoff",
-    "handle": "@richardkalehoff",
-    "avatarURL": "http://localhost:5001/richard.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "handle": "@tylermcginnis",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  },
-  {
-    "id": "pancho",
-    "name": "Panayot Angarev",
-    "handle": "@pangarev",
-    "avatarURL": "http://localhost:5001/pancho.jpg"
-  }
-];
 
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        "id": "karen",
+        "name": "Karen Isgrigg",
+        "handle": "@karen_isgrigg",
+        "avatarURL": "http://localhost:5001/karen.jpg"
+      },
+      {
+        "id": "richard",
+        "name": "Richard Kalehoff",
+        "handle": "@richardkalehoff",
+        "avatarURL": "http://localhost:5001/richard.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "@tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      },
+      {
+        "id": "pancho",
+        "name": "Panayot Angarev",
+        "handle": "@pangarev",
+        "avatarURL": "http://localhost:5001/pancho.jpg"
+      }
+    ]
+  }
   render() {
     return (
         <div>
-          <ListContacts listcontacts = {contacts}/>
+          <ListContacts listcontacts = {this.state.contacts}/>
         </div>
     )
   }
